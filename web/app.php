248183748020887ead5b695470b62a162dcd63bf
LiSinCin <crazy.crazy.eco@gmail.com>
2016-05-10 16:43:03 +0300

    app.php: returning kernel.json overwritten logic

--- web/app.php
+++ web/app.php
@@ -2,6 +2,8 @@
 
 use Symfony\Component\ClassLoader\ApcClassLoader;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Debug\Debug;
+use Modera\DynamicallyConfigurableAppBundle\KernelConfig;
 
 $loader = require __DIR__.'/../app/autoload.php';
 
@@ -13,38 +15,15 @@ $apcLoader = new ApcClassLoader(sha1(__FILE__), $loader);
 $loader->unregister();
 $apcLoader->register(true);
 
-function _mfKernelConfig() {
-    $defaultMode = array(
-        'env' => 'prod',
-        'debug' => false
-    );
-
-    $mode = file_get_contents( './../app/kernel.json');
-
-    if (false == $mode) {
-        return $defaultMode;
-    } else {
-        $mode = json_decode($mode, true);
-        if (is_array($mode) && isset($mode['env']) && isset($mode['debug'])) {
-            return $mode;
-        } else {
-            return $defaultMode;
-        }
-    }
-}
-
 require_once __DIR__.'/../app/AppKernel.php';
 //require_once __DIR__.'/../app/AppCache.php';
 
-$mode = _mfKernelConfig();
-
-if ($mode['env'] == 'dev') {
-    ini_set('display_errors', 1);
+$mode = KernelConfig::read();
+if ($mode['debug']) {
+    Debug::enable();
 }
 
-
 $kernel = new AppKernel($mode['env'], $mode['debug']);
-$kernel->loadClassCache();
 //$kernel = new AppCache($kernel);
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
