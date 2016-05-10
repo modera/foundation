0a62890906e1e133a00009e57272593eec9a6168
LiSinCin <crazy.crazy.eco@gmail.com>
2016-05-10 16:26:44 +0300

    app.php: added support to handle kernel.json. New composer.lock, phpunit.xml.dist correct AppKernel.php adding

--- web/app.php
+++ web/app.php
@@ -1,29 +1,51 @@
 <?php
 
+use Symfony\Component\ClassLoader\ApcClassLoader;
 use Symfony\Component\HttpFoundation\Request;
 
-/**
- * @var Composer\Autoload\ClassLoader
- */
 $loader = require __DIR__.'/../app/autoload.php';
-include_once __DIR__.'/../app/bootstrap.php.cache';
 
-// Enable APC for autoloading to improve performance.
-// You should change the ApcClassLoader first argument to a unique prefix
-// in order to prevent cache key conflicts with other applications
-// also using APC.
-/*
-$apcLoader = new Symfony\Component\ClassLoader\ApcClassLoader(sha1(__FILE__), $loader);
+// Use APC for autoloading to improve performance.
+// Change 'sf2' to a unique prefix in order to prevent cache key conflicts
+// with other applications also using APC.
+
+$apcLoader = new ApcClassLoader(sha1(__FILE__), $loader);
 $loader->unregister();
 $apcLoader->register(true);
-*/
 
-$kernel = new AppKernel('prod', false);
+function _mfKernelConfig() {
+    $defaultMode = array(
+        'env' => 'prod',
+        'debug' => false
+    );
+
+    $mode = file_get_contents( './../app/kernel.json');
+
+    if (false == $mode) {
+        return $defaultMode;
+    } else {
+        $mode = json_decode($mode, true);
+        if (is_array($mode) && isset($mode['env']) && isset($mode['debug'])) {
+            return $mode;
+        } else {
+            return $defaultMode;
+        }
+    }
+}
+
+require_once __DIR__.'/../app/AppKernel.php';
+//require_once __DIR__.'/../app/AppCache.php';
+
+$mode = _mfKernelConfig();
+
+if ($mode['env'] == 'dev') {
+    ini_set('display_errors', 1);
+}
+
+
+$kernel = new AppKernel($mode['env'], $mode['debug']);
 $kernel->loadClassCache();
 //$kernel = new AppCache($kernel);
-
-// When using the HttpCache, you need to call the method in your front controller instead of relying on the configuration parameter
-//Request::enableHttpMethodParameterOverride();
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
