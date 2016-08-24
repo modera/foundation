a75ea2d23208674383059773458c1f2e40de5a6d
Sergei Lissovski <sergei.lissovski@gmail.com>
2016-08-24 14:45:08 +0300

    [MPFE-915] a prorotype integration of new test-runner (phpunit listener's based)

--- phpunit.sh
+++ phpunit.sh
@@ -1,55 +1,31 @@
 #!/usr/bin/env bash
 
-# Experimental, for internal use only
-
-# Internlinks nested composer packages and runs tests
-# Sample usage:
-## Run tests for all packages, before running tests create a temporary MySQL container:
-# ./phpunit.sh --path=src --functional
-## This will only run "foo/bar" package's tests without bootstrapping a temporary MySQL container and linking it with the runner:
-# ./phpunit.sh --path=src --package=foo/bar
-
-RUNNER_GIT_DIR=".mtr"
-
-set -e
+RUNNER_GIT_DIR="mtr"
 
 if ! type docker > /dev/null; then
     echo "Docker is required to run tests."
     exit 1
 fi
 
+if [ ! -d "vendor" ]; then
+  composer install
+fi
+
 if [ ! -d "$RUNNER_GIT_DIR" ]; then
-  git clone git@bitbucket.org:moderasoftware/monolithic-repository-tests-runner.git $RUNNER_GIT_DIR
+  git clone git@bitbucket.org:moderasoftware/tests-runner.git $RUNNER_GIT_DIR
 fi
 
-if [[ "$(docker images -q modera/mtr 2> /dev/null)" == "" ]]; then
-  ROOT_DIR=`pwd`
-
-  cd $RUNNER_GIT_DIR
-  ./install.sh
-
-  cd $ROOT_DIR
-fi
-
-ARGS=$@
-
-if [[ "$ARGS" == "" ]]; then
-  echo ""
-  echo "No args were provided, running tests for all available packages in src/foundation with a temporary MySQL database."
-  echo "Beware, this might take quite a while! You have 15 seconds to abort by doing CTRL+C, waiting ..."
-  echo ""
-
-  sleep 15
-
-  ARGS="--path=src --functional"
+if [[ `docker ps` != *"mtr_mysql"* ]]; then
+  docker run -d -e MYSQL_ROOT_PASSWORD=123123 --name mtr_mysql mysql:5 > /dev/null
 fi
 
 docker run \
-    -it \
-    --rm \
-    -e CWD=`pwd` \
-    -v /var/run/docker.sock:/var/run/docker.sock \
-    -v `which docker`:/usr/bin/docker \
-    -v `pwd`:/mnt/tmp \
-    -w /mnt/tmp \
-    modera/mtr $ARGS
+-it \
+--rm \
+-v `pwd`:/mnt/tmp \
+-w /mnt/tmp \
+--link mtr_mysql:mysql \
+modera/php7-fpm \
+bash -c "vendor/bin/phpunit"
+
+docker rm -f mtr_mysql > /dev/null
