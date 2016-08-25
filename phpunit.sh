ea3c065f718cd81d0523bf8db4063a18038a4d38
Sergei Lissovski <sergei.lissovski@gmail.com>
2016-08-25 13:23:26 +0300

    [MPFE-919] phpunit.sh script improved so it should be possible to run tests now just by running it

--- phpunit.sh
+++ phpunit.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
+# Installs vendors, tests-runner and eventually runs tests
+
 RUNNER_GIT_DIR="mtr"
+
+set -e
 
 if ! type docker > /dev/null; then
     echo "Docker is required to run tests."
@@ -8,14 +12,30 @@ if ! type docker > /dev/null; then
 fi
 
 if [ ! -d "vendor" ]; then
-  composer install
+  echo "# No vendor dir detected, installing modera/composer-monorepo-plugin and then project's dependencies"
+
+  printf "composer global require modera/composer-monorepo-plugin:dev-master\ncomposer install" > install.sh
+  chmod +x install.sh
+
+  docker run \
+  -it \
+  --rm \
+  -v `pwd`:/mnt/tmp \
+  -w /mnt/tmp \
+  modera/php7-fpm "./install.sh"
+
+  rm install.sh
 fi
 
 if [ ! -d "$RUNNER_GIT_DIR" ]; then
-  git clone git@bitbucket.org:moderasoftware/tests-runner.git $RUNNER_GIT_DIR
+  echo "# Cloning test-runner"
+  git clone git@github.com:modera/tests-runner.git $RUNNER_GIT_DIR
 fi
 
+# if there's no mtr_php image then create a Docker file in $RUNNER_GIT_DIR and build it
+
 if [[ `docker ps` != *"mtr_mysql"* ]]; then
+  echo "# Starting database for functional tests"
   docker run -d -e MYSQL_ROOT_PASSWORD=123123 --name mtr_mysql mysql:5 > /dev/null
 fi
 
@@ -28,4 +48,8 @@ docker run \
 modera/php7-fpm \
 bash -c "vendor/bin/phpunit"
 
+EXIT_CODE=$?
+
 docker rm -f mtr_mysql > /dev/null
+
+exit $EXIT_CODE
