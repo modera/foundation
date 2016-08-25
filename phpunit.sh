da53bd3fe2893626846ea4e9e8a90a84c5e8eea4
Sergei Lissovski <sergei.lissovski@gmail.com>
2016-08-25 13:38:35 +0300

    [MPFE-919] bugfix for phpunit.sh, now it should properly install tests-runner

--- phpunit.sh
+++ phpunit.sh
@@ -28,8 +28,16 @@ if [ ! -d "vendor" ]; then
 fi
 
 if [ ! -d "$RUNNER_GIT_DIR" ]; then
-  echo "# Cloning test-runner"
+  echo "# Cloning and installing test-runner"
+
   git clone git@github.com:modera/tests-runner.git $RUNNER_GIT_DIR
+
+  docker run \
+  -it \
+  --rm \
+  -v `pwd`/$RUNNER_GIT_DIR:/mnt/tmp \
+  -w /mnt/tmp \
+  modera/php7-fpm "composer install"
 fi
 
 # if there's no mtr_php image then create a Docker file in $RUNNER_GIT_DIR and build it
