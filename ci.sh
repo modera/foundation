0129cd05e4f2d3efef6e8bde30971931aef8ef0e
Sergei Vizel <sergei.vizel@modera.net>
2019-02-06 11:58:28 +0200

    travis CI improvements

--- ci.sh
+++ ci.sh
@@ -3,6 +3,19 @@
 set -eu
 
 SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+LOCK=NO
+
+for i in "$@"; do
+case $i in
+    --lock)
+        LOCK=YES
+        shift
+    ;;
+    *)
+        # unknown option
+    ;;
+esac
+done
 
 run_tests_with_php() {
     echo ""
@@ -10,7 +23,21 @@ run_tests_with_php() {
     echo ""
 
     sudo rm -rf $SCRIPT_DIR/vendor $SCRIPT_DIR/composer.lock
+
+    if [ "NO" = "$LOCK" ]; then
+        if [ -d $SCRIPT_DIR/vendor.php$1 ]; then
+            sudo cp -rf $SCRIPT_DIR/vendor.php$1 $SCRIPT_DIR/vendor
+        fi
+        if [ -f $SCRIPT_DIR/composer.php$1 ]; then
+            sudo cp $SCRIPT_DIR/composer.php$1 $SCRIPT_DIR/composer.lock
+        fi
+    fi
+
     $SCRIPT_DIR/phpunit.sh --php-version=$1
+
+    sudo rm -rf $SCRIPT_DIR/vendor.php$1
+    sudo cp -rf $SCRIPT_DIR/vendor $SCRIPT_DIR/vendor.php$1
+    sudo cp $SCRIPT_DIR/composer.lock $SCRIPT_DIR/composer.php$1
 }
 
 run_tests_with_php 5.6
