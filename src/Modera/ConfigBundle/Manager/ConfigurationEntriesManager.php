801dc63181cbcb8a7e532570de2bdd035015374d
Sergei Vizel <sergei.vizel@modera.net>
2020-10-09 08:50:36 +0300

    [ModeraConfigBundle] ConfigurationEntriesManager fix

--- src/Modera/ConfigBundle/Manager/ConfigurationEntriesManager.php
+++ src/Modera/ConfigBundle/Manager/ConfigurationEntriesManager.php
@@ -6,6 +6,6 @@ namespace Modera\ConfigBundle\Manager;
  * @author    Sergei Lissovski <sergei.lissovski@modera.org>
  * @copyright 2016 Modera Foundation
  */
-class ConfigurationEntriesManager extends \Modera\ConfigBundle\Config\ConfigurationEntriesManager
+class ConfigurationEntriesManager extends \Modera\ConfigBundle\Config\ConfigurationEntriesManager implements ConfigurationEntriesManagerInterface
 {
 }
