c3153e96c993db1961baa4662a55b5e8badf5d14
Sergei Vizel <sergei.vizel@modera.com>
2025-04-11 16:44:40 +0300

    [ModeraModuleBundle] deprecated

--- src/Modera/ModuleBundle/ModeraModuleBundle.php
+++ src/Modera/ModuleBundle/ModeraModuleBundle.php
@@ -5,6 +5,8 @@ namespace Modera\ModuleBundle;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 /**
+ * @deprecated will be removed in next version
+ *
  * @author    Sergei Vizel <sergei.vizel@modera.org>
  * @copyright 2014 Modera Foundation
  */
